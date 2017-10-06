3ad74fc04fa16c5579d802b8b4e649179235ee78
Wenting Song <swtkelly415@WentingmatoAir.hsd1.wa.comcast.net>
2017-10-05 20:29:31 -0700

    hw1 commit

--- empdata.js
+++ empdata.js
@@ -2,122 +2,122 @@
   "data": [
     {
       "name": "Gaga Winters",
-      "designation": "Accountant",
+      "position": "Accountant",
       "salary": "$180,750",
-      "joining_date": "2012/07/25",
-      "office": "Beijing",
+      "year": "2012/07/25",
+      "country": "Beijing",
       "extension": "8022"
     },
     {
       "name": "Bella Willington",
-      "designation": "Data Specialist",
+      "position": "Data Specialist",
       "salary": "$300,000",
-      "joining_date": "2011/12/02",
-      "office": "Tokyo",
+      "year": "2011/12/02",
+      "country": "Tokyo",
       "extension": "4004"
     },
     {
       "name": "Ash Fox",
-      "designation": "Senior Technical Author",
+      "position": "Senior Technical Author",
       "salary": "$96,000",
-      "joining_date": "2011/11/11",
-      "office": "San Jose",
+      "year": "2011/11/11",
+      "country": "San Jose",
       "extension": "1662"
     },
     {
       "name": "Ali Baba",
-      "designation": "Accountant",
+      "position": "Accountant",
       "salary": "$172,700",
-      "joining_date": "2008/11/28",
-      "office": "Tokyo",
+      "year": "2008/11/28",
+      "country": "Tokyo",
       "extension": "5407"
     },
     {
       "name": "Caesar Great",
-      "designation": "Pre-Sales Lead",
+      "position": "Pre-Sales Lead",
       "salary": "$116,450",
-      "joining_date": "2010/12/12",
-      "office": "New York",
+      "year": "2010/12/12",
+      "country": "New York",
       "extension": "8330"
     },
     {
       "name": "Shannen Docker",
-      "designation": "Regional Director",
+      "position": "Regional Director",
       "salary": "$193,000",
-      "joining_date": "2008/11/13",
-      "office": "London",
+      "year": "2008/11/13",
+      "country": "London",
       "extension": "6443"
     },
     {
       "name": "Cede Kelly",
-      "designation": "Javascript Developer",
+      "position": "Javascript Developer",
       "salary": "$493,060",
-      "joining_date": "2012/03/29",
-      "office": "Ice Land",
+      "year": "2012/03/29",
+      "country": "Ice Land",
       "extension": "2224"
     },
     {
       "name": "Haley Young",
-      "designation": "Senior Graphic Designer",
+      "position": "Senior Graphic Designer",
       "salary": "$413,500",
-      "joining_date": "2011/12/11",
-      "office": "London",
+      "year": "2011/12/11",
+      "country": "London",
       "extension": "3587"
     },
     {
       "name": "Colleen Hush",
-      "designation": "Senior Javascript Developer",
+      "position": "Senior Javascript Developer",
       "salary": "$405,500",
-      "joining_date": "2009/09/15",
-      "office": "Seattle",
+      "year": "2009/09/15",
+      "country": "Seattle",
       "extension": "2000"
     },
     {
       "name": "Dias Rios",
-      "designation": "Division Sales Lead",
+      "position": "Division Sales Lead",
       "salary": "$227,500",
-      "joining_date": "2011/09/26",
-      "office": "Edinburgh",
+      "year": "2011/09/26",
+      "country": "Edinburgh",
       "extension": "2990"
     },
     {
       "name": "Bing Chandler",
-      "designation": "Office Assistant",
+      "position": "Office Assistant",
       "salary": "$237,500",
-      "joining_date": "1998/08/06",
-      "office": "New York",
+      "year": "1998/08/06",
+      "country": "New York",
       "extension": "9998"
     },
     {
       "name": "Becker David",
-      "designation": "Integration Specialist",
+      "position": "Integration Specialist",
       "salary": "$347,900",
-      "joining_date": "2009/10/14",
-      "office": "Osaka",
+      "year": "2009/10/14",
+      "country": "Osaka",
       "extension": "6990"
     },
     {
       "name": "Sonya Forest",
-      "designation": "Software Designer",
+      "position": "Software Designer",
       "salary": "$113,600",
-      "joining_date": "2008/1/13",
-      "office": "Bath",
+      "year": "2008/1/13",
+      "country": "Bath",
       "extension": "1667"
     },
     {
       "name": "Jiji Gaines",
-      "designation": "Office Manager",
+      "position": "Office Manager",
       "salary": "$100,560",
-      "joining_date": "2008/12/19",
-      "office": "Las Vegas",
+      "year": "2008/12/19",
+      "country": "Las Vegas",
       "extension": "4814"
     },
     {
       "name": "Queen Flynn",
-      "designation": "Sales Lead",
+      "position": "Sales Lead",
       "salary": "$342,000",
-      "joining_date": "2012/03/03",
-      "office": "Edinburgh",
+      "year": "2012/03/03",
+      "country": "Edinburgh",
       "extension": "9997"
     }
   ]
